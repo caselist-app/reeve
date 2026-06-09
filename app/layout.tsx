ccfc4b6398dedd08bea48cc150af453db85e64e7
matt-getseer <126175591+caselist-app@users.noreply.github.com>
2026-06-09 15:43:43 +0100

    init

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Geist } from 'next/font/google'
+import './globals.css'
+
+const geist = Geist({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Reeve',
+  description: 'The operating system for tour managers.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={geist.className}>{children}</body>
+    </html>
+  )
+}
